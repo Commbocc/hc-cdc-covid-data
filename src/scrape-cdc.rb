468f4def2b82700c445db9f57ec4751a2192f314
Kevin McCarthy <mccarthyk@hcflgov.net>
2021-07-30 11:52:00 -0400

    include updated_at

--- src/scrape-cdc.rb
+++ src/scrape-cdc.rb
@@ -1,11 +1,21 @@
 require 'net/http'
 require 'json'
 
+puts 'Scraping data'
+
 uri = URI('https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=integrated_county_timeseries_state_FL_external')
+
 data = JSON.parse(Net::HTTP.get(uri))
 
 filtered = data['integrated_county_timeseries_external_data']
-    .select { |x| x['county'] == 'Hillsborough County' }
-    .sort_by { |x| x['date'] }
+.select { |x| x['county'] == 'Hillsborough County' }
+.sort_by { |x| x['date'] }
 
-File.open('public/hc-cdc.json', 'w') { |f| f.write(filtered.to_json) }
+result = {
+    updated_at: Time.new,
+    data: filtered
+}
+
+File.open('public/hc-cdc.json', 'w') { |f| f.write(result.to_json) }
+
+puts 'Finished scraping'
